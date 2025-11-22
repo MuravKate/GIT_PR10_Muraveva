5b6cf41e94fa4155d7bd1d0b3af380295365c335
Kate <kate09102007@icloud.com>
2025-11-22 20:51:30 +0300

    коммит 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine($"Текущая дата: {DateTime.Now:dd.MM.yyyy}");
+Console.WriteLine($"Группа: ИС-23А");
+Console.WriteLine($"Муравьева Катя);
